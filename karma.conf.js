4b2fe7001d22dc64c50bd811596190d57a1dea4b
ohad <ohadnav@gmail.com>
2016-12-13 17:04:10 +0200

    Removed karma-coverage

--- karma.conf.js
+++ karma.conf.js
@@ -27,14 +27,14 @@ module.exports = function (config) {
             // preprocess matching files before serving them to the browser
             // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
             preprocessors: {
-                '**/*.js': ['coverage', 'webpack']
+                '**/*.js': ['webpack']
             },
 
 
             // test results reporter to use
             // possible values: 'dots', 'progress'
             // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-            reporters: ['progress', 'mocha', 'coverage'],
+            reporters: ['progress', 'mocha'],
 
 
             // web server port
