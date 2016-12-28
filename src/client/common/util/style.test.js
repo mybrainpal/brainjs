3e9bce35d52a2f8d3af926595612d7db0855e3da
ohad <ohadnav@gmail.com>
2016-12-28 17:36:46 +0200

    removed unused npm

--- src/client/common/util/style.test.js
+++ src/client/common/util/style.test.js
@@ -1,35 +1,44 @@
 /**
  * Proudly created by ohad on 15/12/2016.
  */
-var chai      = require('chai'),
-    expect    = require('chai').expect,
-    rewire    = require('rewire'),
-    StyleUtil = rewire('./style');
+const chai      = require('chai'),
+      expect    = require('chai').expect,
+      StyleUtil = require('./style'),
+      css       = require('./testdata/style.css'),
+      scss      = require('./testdata/test.scss');
 
-describe('StyleUtil', function () {
-    var a1;
-    before(function () {
+describe.only('StyleUtil', function () {
+    let a1;
+    before(() => {
         a1             = document.createElement('a');
         a1.textContent = 'plata o plomo'; // for testing and life
         a1.setAttribute('href', 'https://www.youtube.com/watch?v=HdCvg17P6FU&t=36');
         a1.setAttribute('id', 'a1');
         document.getElementsByTagName('body')[0].appendChild(a1);
     });
-    it('Load cssText from import', function () {
-        var styles = require('css!./testdata/style.css');
-        a1.setAttribute('class', styles.locals.stark);
-        StyleUtil.load(styles[0][1]);
+    it('Load css from import', () => {
+        a1.setAttribute('class', css.locals.stark);
+        StyleUtil.load(css[0][1]);
         expect(window.getComputedStyle(a1).paddingLeft).to.equal('100px');
         expect(window.getComputedStyle(a1).paddingTop).to.equal('50px');
     });
-    afterEach(function () {
+    it('Load scss from import', () => {
+        a1.setAttribute('class', scss.locals.test);
+        StyleUtil.load(scss[0][1]);
+        expect(window.getComputedStyle(a1).marginRight).to.equal('10px');
+    });
+    it('Load text', () => {
+        StyleUtil.load('a {margin-left: 10px}');
+        expect(window.getComputedStyle(a1).marginLeft).to.equal('10px');
+    });
+    afterEach(() => {
         document.querySelectorAll('style[' + StyleUtil.identifyingAttribute + ']')
                 .forEach(function (styleElement) {
                     styleElement.parentNode.removeChild(styleElement);
                 });
         a1.setAttribute('class', '');
     });
-    after(function () {
+    after(() => {
         a1.parentNode.removeChild(a1);
     });
 });
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+    entry   : './src/client/index.js',
+    output  : {
+        filename: 'brain.js',
+        path    : './dist'
+    },
+    devtools: 'source-map',
+    resolve : {
+        extensions: ['.js', '.jsx', '.css', '.scss', '']
+    },
+    module  : {
+        loaders: [
+            {
+                test   : /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader : 'babel-loader',
+                query  : {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test   : /\.s?css/,
+                loaders: [
+                    'css?sourceMap&modules&importLoaders=1&localIdentName=[name]--[hash:base64:3]',
+                    'sass?sourceMap&localIdentName=[name]--[hash:base64:3]'
+                ],
+                exclude: /node_modules|lib/
+            }
+        ]
+    },
+    plugins : []
+};
+// webpack --config webpack.config.js --optimize-minimize --optimize-dedupe
