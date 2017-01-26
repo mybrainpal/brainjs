e91fa0ed40fa24e8a66478c4d42c643ab23ceb62
ohad <ohadnav@gmail.com>
2017-01-26 19:41:23 +0200

    test

--- src/server/auth/auth.js
+++ src/server/auth/auth.js
@@ -8,21 +8,39 @@ const Customer = require('./customer');
  * @returns {Promise} that is resolved if the provided credentials should be approved, or
  * rejected otherwise.
  */
+// exports.auth = function (customer) {
+//   return new Promise(function (resolve, reject) {
+//     Customer.findOne({apiKey: customer.apiKey}, function (error, _customer) {
+//       if (error) {
+//         reject(error);
+//       }
+//       if (_customer) {
+//         if (customer.url && _customer.url && _customer.url.indexOf(customer.url) === -1 &&
+//             customer.url.indexOf(_customer.url) === -1) {
+//           console.warn(
+//             `Invalid referer: ${customer.url} FOR ${_customer.name} (${_customer.apiKey})`);
+//         }
+//         resolve(_customer);
+//       }
+//       reject();
+//     });
+//   });
+// };
 exports.auth = function (customer) {
   return new Promise(function (resolve, reject) {
-    Customer.findOne({apiKey: customer.apiKey}, function (error, _customer) {
-      if (error) {
-        reject(error);
-      }
-      if (_customer) {
-        if (customer.url && _customer.url && _customer.url.indexOf(customer.url) === -1 &&
-            customer.url.indexOf(_customer.url) === -1) {
-          console.warn(
-            `Invalid referer: ${customer.url} FOR ${_customer.name} (${_customer.apiKey})`);
-        }
-        resolve(_customer);
-      }
-      reject();
-    });
+    // Customer.findOne({apiKey: customer.apiKey}, function (error, _customer) {
+    //   if (error) {
+    //     reject(error);
+    //   }
+    //   if (_customer) {
+    //     if (customer.url && _customer.url && _customer.url.indexOf(customer.url) === -1 &&
+    //         customer.url.indexOf(_customer.url) === -1) {
+    //       console.warn(
+    //         `Invalid referer: ${customer.url} FOR ${_customer.name} (${_customer.apiKey})`);
+    //     }
+    resolve(customer);
+    //     }
+    //     reject();
+    //   });
   });
 };
