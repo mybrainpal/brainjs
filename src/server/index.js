fa4a458ead1e2d2bad6d509869d5da39c56a46c0
ohad <ohadnav@gmail.com>
2017-01-27 05:11:11 +0200

    remove serving log

--- src/server/index.js
+++ src/server/index.js
@@ -34,8 +34,6 @@ app.get('/serve/?:name/?:apiKey/brain.js', (request, response) => {
             path.join(distPath, `${Auth.isDev(request) ? 'dev/' : ''}${actualCustomer.name}.js`);
     fs.exists(brainJsPath, (exists) => {
       if (exists) {
-        console.log(`Serving ${Auth.isDev(request) ? 'DEV ' : ''}` +
-                    `${brainJsPath} for ${actualCustomer.name}`);
         response.sendFile(brainJsPath);
       } else {
         console.error('Bad news.. could not find brain.js at ' + brainJsPath);
