15c9e2c38a4b1ca5a0233917ecc29e27ab027eda
ohad <ohadnav@gmail.com>
2017-01-28 20:08:12 +0200

    private search

---
+++ src/client/configurations/dev/private-search.js
@@ -0,0 +1,12 @@
+/**
+ * Proudly created by ohad on 28/01/2017.
+ */
+const Play = require('../../play');
+
+const configuration = {
+  storage: {
+    name: 'google-analytics'
+  }
+};
+
+Play(configuration);
