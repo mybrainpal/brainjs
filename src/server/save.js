b9a5b2d9bb231125c33cf11da15005801dd146e6
ohad <ohadnav@gmail.com>
2017-02-16 18:06:44 +0200

    test

--- src/server/save.js
+++ src/server/save.js
@@ -1,16 +1,25 @@
 /**
  * Proudly created by ohad on 14/02/2017.
  */
-const router = require('express').Router();
+const router     = require('express').Router(),
+      bodyParser = require('express').bodyParser();
 
 router.param('message', function (req, res, next, name) {
   console.log('validating ' + name);
-  console.log('value: ' + req.param(name));
+  console.log('value: ' + req.params(name));
   next();
 });
 
-router.post('/:message', function (req, res) {
-  console.log('processing: ' + req.param.message);
+router.use('/', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+router.use(bodyParser);
+
+router.post('/', function (req, res) {
+  console.log('processing: ' + req.body.message);
   res.status(200);
   res.send('');
 });
