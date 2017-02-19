9f6d880582df990a6587a76f7b462922c8460c56
ohad <ohadnav@gmail.com>
2017-02-19 18:03:20 +0100

    geektime

---
+++ src/client/configurations/geektime.js
@@ -0,0 +1,16 @@
+/**
+ * Proudly created by ohad on 19/02/2017.
+ */
+const Play    = require('../play'),
+      Storage = require('../common/storage/storage');
+
+const configuration = {
+  storage: {
+    name   : Storage.names.GOOGLE_ANALYTICS,
+    options: {
+      trackingId: 'UA-91064115-6'
+    }
+  }
+};
+
+Play(configuration);
