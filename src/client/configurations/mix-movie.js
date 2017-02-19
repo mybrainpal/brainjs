c0fa93f258df20012aa5d65baf37d4aaf72d6a26
ohad <ohadnav@gmail.com>
2017-02-19 18:07:57 +0100

    mix movie

--- src/client/configurations/mix-movie.js
+++ src/client/configurations/mix-movie.js
@@ -23,44 +23,9 @@ const configuration = {
           {
             label       : 'watching',
             demographics: [{
-              name: Demographics.PROPERTIES.MODULO.name, moduloIds: [0], moduloOf: 5
+              name: Demographics.PROPERTIES.MODULO.name, moduloIds: [0], moduloOf: 1
             }],
             executors   : [
-              {name: StyleExecutor.name, options: {css: require('./alertify.css')}},
-              {
-                name   : AlertifyInterface.name,
-                options: {
-                  alertifyFn: _createFn(Math.round(5 + Math.random() * 30) +
-                                        ' are watching now.')
-                }
-              }
-            ]
-          },
-          {
-            label       : 'no-menu',
-            demographics: [{
-              name: Demographics.PROPERTIES.MODULO.name, moduloIds: [1], moduloOf: 10
-            }],
-            executors   : [
-              {name: StyleExecutor.name, options: {css: 'nav{display:none}'}},
-            ]
-          },
-          {
-            label       : 'no-footer',
-            demographics: [{
-              name: Demographics.PROPERTIES.MODULO.name, moduloIds: [2], moduloOf: 10
-            }],
-            executors   : [
-              {name: StyleExecutor.name, options: {css: '.categories{display:none}'}},
-            ]
-          },
-          {
-            label       : 'combined',
-            demographics: [{
-              name: Demographics.PROPERTIES.MODULO.name, moduloIds: [3], moduloOf: 10
-            }],
-            executors   : [
-              {name: StyleExecutor.name, options: {css: 'nav,.categories{display:none}'}},
               {name: StyleExecutor.name, options: {css: require('./alertify.css')}},
               {
                 name   : AlertifyInterface.name,
@@ -74,11 +39,9 @@ const configuration = {
         ]
       },
       options   : {
-        subjectOptions: {
-          anchor: {
-            selector: 'a.modal-button.install-button',
-            event   : 'click'
-          }
+        anchor: {
+          selector: 'a.modal-button.install-button',
+          event   : 'click'
         }
       }
     }
