09f02865469b0f9fffaae44990e2de7d7c9680b4
ohad <ohadnav@gmail.com>
2017-03-11 22:50:17 +0200

    lubinsky

--- src/client/configurations/lubinsky.js
+++ src/client/configurations/lubinsky.js
@@ -1,8 +1,15 @@
 /**
  * Proudly created by ohad on 10/03/2017.
  */
-const Play              = require('../play'),
+const _                 = require('../common/util/wrapper'),
+      Play              = require('../play'),
+      Factory           = require('../common/events/factory'),
+      IdleEvent         = require('../common/events/idle'),
+      WordEvent         = require('../common/events/word'),
+      Master            = require('../manipulation/execute/master'),
+      EventExecutor     = require('../manipulation/execute/interaction/event'),
       AlertifyInterface = require('../manipulation/execute/interface/alertify'),
+      SwalInterface     = require('../manipulation/execute/interface/sweetalert'),
       StyleExecutor     = require('../manipulation/execute/dom/style'),
       Demographics      = require('../manipulation/experiment/demographics'),
       Storage           = require('../common/storage/storage');
@@ -47,6 +54,139 @@ Play(
             }
           ]
         }
+      },
+      {
+        experiment: {
+          id          : 2,
+          label       : 'modal',
+          demographics: [{name: Demographics.PROPERTIES.RESOLUTION.name, minWidth: 800}],
+          groups      : [
+            {
+              label    : 'modal',
+              executors: [
+                {
+                  name   : StyleExecutor.name,
+                  options: {css: `[class^='swal2'] { direction: rtl}`}
+                },
+                {
+                  name   : SwalInterface.name,
+                  options: {
+                    on    : true,
+                    swalFn: (swal) => {
+                      swal.setDefaults(
+                        {
+                          allowEscapeKey   : false,
+                          showConfirmButton: true,
+                          showCancelButton : false,
+                          allowOutsideClick: false,
+                          allowEnterKey    : true
+                        });
+                      //noinspection JSUnusedGlobalSymbols
+                      swal.queue([{
+                        type             : 'success',
+                        title            : 'לקבלת עד 40% הנחה',
+                        text             : 'לאיזה מספר לחזור אליך?',
+                        input            : 'tel',
+                        confirmButtonText: 'דברו איתי &larr;',
+                        preConfirm       : function (tel) {
+                          return new Promise(function (resolve, reject) {
+                            if (/^0[0-9]{9}$/.test(tel)) {
+                              resolve(tel);
+                            } else {
+                              reject('מספר לא חוקי')
+                            }
+                          })
+                        }
+                      }, {
+                        type             : 'question',
+                        title            : 'רק עוד קצת..',
+                        text             : 'איך קוראים לך?',
+                        input            : 'text',
+                        confirmButtonText: 'קליק וסיימנו &larr;',
+                        preConfirm       : function (name) {
+                          return new Promise(function (resolve, reject) {
+                            if (/.+/.test(name)) {
+                              resolve(name);
+                            } else {
+                              reject('חשוב לנו לדעת מה שמך')
+                            }
+                          })
+                        }
+                      }, {
+                        type             : 'question',
+                        title            : 'איפה נוח לך?',
+                        text             : 'בחר/י את הסניף הקרוב לנסיעת מבחן',
+                        input            : 'select',
+                        inputOptions     : {
+                          '0': 'חיפה',
+                          '1': 'תל אביב',
+                          '2': 'ירושלים'
+                        },
+                        confirmButtonText: 'שלח/י',
+                        onOpen           : () => {
+                          setTimeout(
+                            () => {document.querySelector('.swal2-modal.swal2-show').focus()});
+                        }
+                      }]).catch(swal.noop)
+                          .then(function (response) {
+                            const formData = {
+                              name: response[1],
+                              tel : response[0],
+                              city: response[2]
+                            };
+                            _sendForm(formData);
+                          });
+                      new WordEvent({
+                        waitTime: 500, enforceRegex: true, regex: /^0[0-9]{9}$/,
+                        target                                  : 'input.swal2-input'
+                      });
+                      _.on(Factory.eventName(WordEvent.name()),
+                           () => {
+                             swal.clickConfirm();
+                             new WordEvent({waitTime: 1000, target: 'input.swal2-input'});
+                             _.on(Factory.eventName(WordEvent.name()),
+                                  () => {
+                                    swal.clickConfirm();
+                                  }, {}, 'input.swal2-input');
+                           }, {}, 'input.swal2-input');
+                    }
+                  }
+                },
+                {
+                  name   : EventExecutor.name,
+                  options: {
+                    create: {
+                      event  : IdleEvent.name(),
+                      options: {
+                        detailOrId: 'swal',
+                        waitTime  : 5000
+                      }
+                    }
+                  }
+                },
+                {
+                  name   : EventExecutor.name,
+                  options: {
+                    listen : {
+                      event     : Factory.eventName(IdleEvent.name()),
+                      detailOrId: 'swal'
+                    },
+                    trigger: {
+                      event: Master.eventName(SwalInterface.name)
+                    }
+                  }
+                }
+              ]
+            }
+          ]
+        }
       }
     ]
   });
+
+function _sendForm(formData) {
+  document.querySelector('#Input_textbox_0').value  = formData.name;
+  document.querySelector('#Input_textbox_1').value  = formData.tel;
+  document.querySelector('#Input_ndropbox_0').value = formData.city;
+  _.trigger('click', {}, '#Button_submitButton_0');
+}
